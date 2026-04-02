d1211e32fb0772683e13abb81e122f102f9f40cd
Enock <enock.kibet@student.moringaschool.com>
2026-04-02 22:32:14 +0300

    first push

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const userInfo = {
+  "username" : "user1",
+  "password" : "password123"
+}
+function createLoginTracker(userInfo) {
+      let attemptCount = 0;
+      const Maxattempts = 3;
+      let passwordAttempt = prompt("Enter your password");
+      // the user is prompted to input the password
+      return (passwordAttempt) => {
+        attemptCount++;
+        // The function adds onto the counter
+
+        if (attemptCount > Maxattempts) {
+            return "Max attempts reached login failure"
+        }
+         // if the max number of tries has been reached the system refuses to validate the password
+        if (passwordAttempt === userInfo.password) {
+          return "Login successful"
+        } else if (passwordAttempt !== userInfo.password) {
+          return "login failed. Attempts left are ${Maxattempts - attemptCount}` "
+        } 
+      };
 
 
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+}
