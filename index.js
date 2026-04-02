ae52d10e23255e777dedcb2544f7c9d085bb557e
Enock <enock.kibet@student.moringaschool.com>
2026-04-02 22:47:05 +0300

    Changed some things

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const userInfo = {
 function createLoginTracker(userInfo) {
       let attemptCount = 0;
       const Maxattempts = 3;
-      let passwordAttempt = prompt("Enter your password");
-      // the user is prompted to input the password
       return (passwordAttempt) => {
-        attemptCount++;
+          attemptCount++;
+         if (attemptCount > Maxattempts) {
+            return "Account locked"
+        }
+      
         // The function adds onto the counter
 
-        if (attemptCount > Maxattempts) {
-            return "Max attempts reached login failure"
-        }
+       
          // if the max number of tries has been reached the system refuses to validate the password
         if (passwordAttempt === userInfo.password) {
           return "Login successful"
